5a65105376a2422421ba561929a9acf7415ba62f
Jorge <kokybarba@gmail.com>
2026-05-22 17:38:59 -0500

    aprendi sobre constantes y tambien condicionales con if else

---
+++ 09constante.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (){
+    const string nombre = "jorge";
+    // las constantes no se pueden sobre escribir, ejemplo: nombre = "ricardo";
+    cout << nombre ;
+    return 0;
+}
